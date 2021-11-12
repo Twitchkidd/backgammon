1a79c3f98aef4c8f61670d6318affe6f9b33be0d
Gareth Field <field.gareth@gmail.com>
2021-11-12 08:09:50 -0500

    first run, lol!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-function App() {
-	return <alldivallday />;
-}
+import { useEffect, useState } from 'react';
+import Game from './components/Game';
+import Main from './components/Main';
+
+const App = () => {
+	const [gameState, setGameState] = useState('flippin sweet');
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (gameState !== 'extra amazing') {
+				setGameState('extra amazing');
+			}
+		}, 1500);
+		return clearTimeout(timer);
+	}, [gameState]);
+	return (
+		<Main>
+			<Game state={gameState} />
+		</Main>
+	);
+};
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Main = styled.main`
+	width: 100vw;
+	height: 100vh;
+	display: grid;
+	place-items: center;
+`;
+
+export default Main;
